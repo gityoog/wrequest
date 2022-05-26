74756616057c8854fe7343cddfa2b8119cdc9293
yoog <907850084@qq.com>
2022-05-26 16:38:59 +0800

    feat: add result

--- src/generator/index.ts
+++ src/generator/index.ts
@@ -1,15 +1,19 @@
 type callback<T> = () => Promise<T>
 type beforeCallback = () => Promise<void>
 type result<T> = { type: 'success', data: T } | { type: 'fail', error: unknown }
+type afterCallback<T> = (result: result<T>) => Promise<void>
 
 namespace PromiseGenerator {
   export type Callback<T> = callback<T>
   export type BeforeCallback = beforeCallback
+  export type AfterCallback<T> = afterCallback<T>
+  export type Result<T> = result<T>
 }
 
 class PromiseGenerator<T> {
   private callback?: callback<T>
   private beforeCallbacks: Array<beforeCallback> = []
+  private afterCallbacks: Array<afterCallback<T>> = []
 
   set(callback: callback<T>) {
     this.callback = callback
@@ -19,6 +23,9 @@ class PromiseGenerator<T> {
     this.beforeCallbacks.push(callback)
   }
 
+  after(callback: afterCallback<T>) {
+    this.afterCallbacks.push(callback)
+  }
   async run() {
     for (const beforeCallback of this.beforeCallbacks) {
       await beforeCallback()
@@ -30,12 +37,16 @@ class PromiseGenerator<T> {
     } catch (e) {
       result = { type: 'fail', error: e }
     }
+    for (const afterCallback of this.afterCallbacks) {
+      await afterCallback(result)
+    }
     return result
   }
 
   destroy() {
     this.callback = undefined
     this.beforeCallbacks = []
+    this.afterCallbacks = []
   }
 }
 
