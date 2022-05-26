27f7801f4f61fb14e37d02ad66b5533a8d121cd8
yoog <907850084@qq.com>
2022-05-26 16:38:36 +0800

    feat: add result

--- src/callback/abort/index.ts
+++ src/callback/abort/index.ts
@@ -1,16 +1,17 @@
+import PromiseGenerator from "../../generator"
 
 namespace AbortCallback {
-  export type Callback = (() => Promise<boolean | void>) | (() => boolean | void)
+  export type Callback<T> = ((result: PromiseGenerator.Result<T>) => Promise<boolean | void>) | ((result: PromiseGenerator.Result<T>) => boolean | void)
 }
 
-class AbortCallback {
-  private callbacks: AbortCallback.Callback[] = []
-  add(callback: AbortCallback.Callback) {
+class AbortCallback<T> {
+  private callbacks: AbortCallback.Callback<T>[] = []
+  add(callback: AbortCallback.Callback<T>) {
     this.callbacks.push(callback)
   }
-  async run() {
+  async run(result: PromiseGenerator.Result<T>) {
     for (const callback of this.callbacks) {
-      if (await callback() === true) {
+      if (await callback(result) === true) {
         return true
       }
     }
